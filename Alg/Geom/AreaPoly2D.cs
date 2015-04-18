65d3ff42c227dbe578deed980e1e3bbe09a36abe
Petar Ivanov <psivanov@gmail.com>
2015-04-17 22:00:26 -0700

    add overload for area computation

--- Alg/Geom/AreaPoly2D.cs
+++ Alg/Geom/AreaPoly2D.cs
@@ -8,9 +8,35 @@ namespace Utils
     public partial class Alg
     {
         /// <summary>
-        /// Anticlockwise oriented area.
+        /// Anticlockwise oriented area multiplied by 2.
+        /// Points are packed in array of pairs.
         /// </summary>
-        public static double AreaPoly2D(params double[] points)
+        public static double Area2Poly2D(params double[][] points)
+        {
+            double area = 0;
+            double x1, y1, x2, y2;
+            int n = points.Length;
+            if (n > 2)
+            {
+                x2 = points[n-1][0];
+                y2 = points[n-1][1];
+                for (int i = 0; i < n; i++)
+                {
+                    x1 = x2;
+                    y1 = y2;
+                    x2 = points[i][0];
+                    y2 = points[i][1];
+                    area += (y1 + y2) * (x1 - x2);
+                }
+            }
+            return area;
+        }
+
+        /// <summary>
+        /// Anticlockwise oriented area multiplied by 2.
+        /// Points are packed in a single array.
+        /// </summary>
+        public static double Area2Poly2D(params double[] points)
         {
             double area = 0;
             double x1, y1, x2, y2;
@@ -25,7 +51,7 @@ namespace Utils
                     y1 = y2;
                     x2 = points[2 * i];
                     y2 = points[2 * i + 1];
-                    area += (y1 + y2) * (x1 - x2) / 2;
+                    area += (y1 + y2) * (x1 - x2);
                 }
             }
             return area;
